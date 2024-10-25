3a60c8bc97ee34ec4ceced8196d360b0e32aa51c
minji2219 <200146@jnu.ac.kr>
2024-10-25 17:53:29 +0900

    feat: 모집 완료시 모달 생성
    모집이 완료되면 뜨는 모달 생성

--- src/components/spot/map/index.tsx
+++ src/components/spot/map/index.tsx
@@ -7,18 +7,24 @@ import { Common } from '@styles/globalStyle';
 import Modal from '@components/common/Modal';
 import RecruitDialog from '../RecruitDialog';
 import { SearchSpotProvider } from '@provider/SearchSpot';
+import AlertDialog from '@components/common/Modal/AlertDialog';
+import { useNavigate } from 'react-router-dom';
+import { RouterPath } from '@routes/path';
 
 const KakaoMap = () => {
   const { location } = useContext(LocationContext);
+  const navigate = useNavigate();
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [recruitIsOpen, setRecruitIsOpen] = useState(false);
+  const [completeModalIsOpen, setCompleteModalIsOpen] = useState(false);
+
   return (
     <Map
       center={{ lat: location.lat, lng: location.lng }}
       style={{ width: '100%', height: '100%' }}
       level={3}
     >
-      <PlusBtnIcon onClick={() => setIsOpen(true)}>
+      <PlusBtnIcon onClick={() => setRecruitIsOpen(true)}>
         <BsPlusCircleFill size="50" />
       </PlusBtnIcon>
 
@@ -30,13 +36,41 @@ const KakaoMap = () => {
       />
       <Modal
         size="big"
-        isOpen={isOpen}
-        onRequestClose={() => setIsOpen(false)}
+        type="transparent"
+        isOpen={recruitIsOpen}
+        onRequestClose={() => setRecruitIsOpen(false)}
         title="모집"
         content={
           <SearchSpotProvider>
-            <RecruitDialog onRequestClose={() => setIsOpen(false)} />
+            <RecruitDialog
+              onRequestClose={() => setRecruitIsOpen(false)}
+              onRequestConfirm={() => setCompleteModalIsOpen(true)}
+            />
           </SearchSpotProvider>
+        }
+      />
+      <Modal
+        type="complete"
+        isOpen={completeModalIsOpen}
+        onRequestClose={() => setCompleteModalIsOpen(false)}
+        title="완료"
+        content={
+          <AlertDialog
+            type="complete"
+            content={
+              <div>
+                스팟 생성이 완료됐습니다.
+                <br />
+                스팟 내역은 마이페이지에서 확인할 수 있습니다.
+                <br />
+                이동하시겠습니까?
+              </div>
+            }
+            onRequestClose={() => setCompleteModalIsOpen(false)}
+            onRequestConfirm={() => {
+              navigate(RouterPath.myPageOrderDetail);
+            }}
+          />
         }
       />
     </Map>
